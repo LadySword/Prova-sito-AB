02b4f7812922bedae61bd8ee8a3a412ea2f22c0f
LadySword <b.spada@mash.it>
2018-09-07 16:02:49 +0200

    Modifiche 7sett2018
    Aggiunti JS che non funziona, migliorato html e css

--- ab-js-file.js
+++ ab-js-file.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 
+
+$(document).ready(function(){
+	"use strict";
+$("a").hover(function(){
+	$(this).css("color", "#4d4d4d");
+}, function(){
+    $(this).css("color", "#999");
+});
+	
+});
